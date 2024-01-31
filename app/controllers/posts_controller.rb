3d5c685b8a1a1742891e46385ce6774c6ec4e0aa
Vipul Tandon <vipul.tandon@metafic.co>
2024-02-01 02:09:17 +0530

    Added serializers to render custom json

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,28 +1,51 @@
 class PostsController < ApplicationController
     before_action :authorize_request
+    before_action :find_post, except: [:create, :index]
 
     def create
-        post = Post.new(post_params)
-        if post.save
-            post.file.attach(params[:file])
-            file_url = url_for(post.file)
-            render json: { status: 'success', message: 'File uploaded successfully', post: post, file_url: file_url }
+        @post = current_user.posts.new(post_params)
+        if @post.save
+            @post.file.attach(params[:file])
+            render json: format_output(@post), status: :ok
         else
-            render json: { status: 'error', message: 'Failed to upload file' }
+            render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
         end
     end
 
+    def index
+        @posts = current_user.posts
+        render json: @posts, status: :ok
+    end
+    
     def show
-        post = current_user.posts.find(params[:id])
-        file_url = url_for(post.file)
-        render json: { status: 'success', post: post, file_url: file_url }
-        rescue ActiveRecord::RecordNotFound
-        render json: { status: 'error', message: 'Post not found' }
+        render json: @post, status: :ok
+    end
+
+    def destroy
+        @post.destroy
+        head :no_content
     end
     
     private
+        def find_post
+            @post = current_user.posts.find_by(id: params[:id])
+            if @post
+                return @post
+            else
+                render json: { errors: 'Post not found' }, status: :not_found
+            end
+        end
+    
+        def format_output(post)
+            file_url = url_for(post.file)
+            {
+                post: post,
+                file_url: file_url
+            }
+        end
 
-    def post_params
-        params.require(:post).permit(:title, :file)
-    end
+        def post_params
+            params.require(:post).permit(:title, :file)
+        end
+        
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,14 +14,35 @@ class UsersController < ApplicationController
 
     def index
         @users = User.all
-        render json: @users, status: :ok
+        render json: @users, each_serializer: UserSerializer, status: :ok
+    end
+    
+    def show
+        @user = User.find_by(id: params[:id])
+        render json: @user, serializer: UserSerializer, show_posts: true, status: :ok
+    end
+
+
+    def destroy
+        @user = User.find_by(id: params[:id])
+        @user.destroy
+        head :no_content
     end
 
 
     private
     def user_params
-        params.permit(
-        :username, :email, :password, :password_confirmation
-        )
+        params.permit( :username, :email, :password, :password_confirmation )
+    end
+
+    def show_user_count(users)
+        users = User.all.map do |user| {
+            id: user.id,
+            username: user.username,
+            email: user.email,
+            post_count: user.posts.count
+        }
+        end
+        return users
     end
 end
